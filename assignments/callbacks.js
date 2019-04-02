b7d7d50cd3257c777b7896b72a9195bbdc630cad
Shobhita Vaishnav <to.shobhita@gmail.com>
2019-04-01 17:56:29 -0700

    MVP complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,25 +27,52 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr);
 }
+function myArrLength(arey){
+  console.log(arey.length);
+}
+getLength(items,myArrLength);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+return cb(arr);
 }
-
+function myLastValue(arey){
+console.log(arey[arey.length-1]);
+}
+last(items,myLastValue);
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x,y);
 }
-
+function add(a,b){
+  return a+b;
+}
+console.log(sumNums(5,6,add));
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x,y);
 }
+function multiply(a,b){
+  return a*b;
+}
+console.log(multiplyNums( 3,7,multiply));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(item,list);
 }
-
+ function checkItems(myItem,arr){
+  for(i=0;i<arr.length;i++){
+    if(myItem == arr[i]){
+     return true; 
+    } 
+  }
+  return false;
+}
+console.log(contains('Pen',items,checkItems));
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
